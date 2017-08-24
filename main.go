9ec6bee262ab55e5c89f2bd680021863b92cb938
CyberMergina <cyber.mergina@gmail.com>
2017-08-25 02:11:08 +0900

    HTMLテンプレートをつかう

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"net/http"
 	"log"
+	"path/filepath"
+	"html/template"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte(`Hello, World`))
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func(){
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "index.html"})
+
 	if err:=http.ListenAndServe(":8080", nil); err!= nil {
 		log.Fatal("ListenAndServe",err)
 	}
