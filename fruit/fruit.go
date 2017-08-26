c3512ffc8a239df982a4a30fe33b0ff52628b15f
CyberMergina <cyber.mergina@gmail.com>
2017-08-26 16:40:39 +0900

    JSONファイルの読み込み

--- fruit/fruit.go
+++ fruit/fruit.go
@@ -1,7 +1,9 @@
 package fruit
 
 import (
-	"fmt"
+	"log"
+	"io/ioutil"
+	"encoding/json"
 )
 
 type Product struct {
@@ -12,13 +14,29 @@ type Data struct {
 	Products	[]*Product
 }
 
+var dataFile = "fruit/data.json" // go runを実行した場所から対象のjsonファイルへのパス
+
 func init() {
-	fmt.Println("include fruit.")
+	log.Println("include fruit.")
 }
 
 func GetList() Data {
-	p1 := Product{Name:"りんご"}
-	p2 := Product{Name:"なし"}
-	p3 := Product{Name:"ばなな"}
-	return Data{Products: []*Product{ &p1, &p2, &p3}}
+	// ioutilパッケージのReadFile関数にファイルパスを渡してファイルを読む
+	// 戻り値が「ファイルの中身」と「エラー」の２つ。カンマ区切りで変数を並べて受け取れる
+	bytes, err := ioutil.ReadFile(dataFile)
+	// err変数が空でなかった場合
+	if err != nil {
+		// ログを出力する
+		log.Fatalf("%s", err)
+	}
+	// 空のlist変数を用意する
+	var list []*Product
+	// bytesに格納されたJSON文字列を、構造体へ変換しlistに格納
+	// err変数が空でなかった場合
+	if err := json.Unmarshal(bytes, &list); err != nil {
+		// ログを出力する
+		log.Fatal(err)
+	}
+	// Dataの構造体のProductsにjsonファイルから取得したデータセットして返す
+	return Data{Products: list}
 }
